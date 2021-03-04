a9305cc91b31c52a7750e258c806ade5eb5baf53
Tunahan <tunahan.can@hotmail.com.tr>
2021-03-04 20:57:55 +0300

    first commit

---
+++ src/main/java/com/example/Notes.java
@@ -0,0 +1,23 @@
+package com.example;
+
+
+
+
+/**
+ * 
+ *  @author tnhncn
+ * 
+ *  I n v er s i o n o f Co n t r o l ( I o C)
+ *
+ */
+
+
+public class Notes {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("merhaba");
+		
+	}
+}
